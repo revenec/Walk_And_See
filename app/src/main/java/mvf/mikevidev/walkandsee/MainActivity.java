c9407e8a08d7420e603c5d4f136ec1364a918efa
Miquel Vives <miquelvivesfont@hotmail.es>
2022-01-02 18:46:30 +0000

    Change login process to check if the account is created by requesting credentials

--- app/src/main/java/mvf/mikevidev/walkandsee/MainActivity.java
+++ app/src/main/java/mvf/mikevidev/walkandsee/MainActivity.java
@@ -17,11 +17,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import mvf.mikevidev.walkandsee.UtilitiesWalkAndSee.Utilities;
 import mvf.mikevidev.walkandsee.viewmodels.SearchPlacesActivity;
